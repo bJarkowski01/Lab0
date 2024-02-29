9314f1fe1bbf6124ccb8294bb3ac6f5067f10422
barte <barte@LAPTOP-GA3N7E1H>
2024-02-29 22:23:29 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class FizzBuzz
+    {
+        int uB;
+        public FizzBuzz(int upper)
+        {
+            uB = upper;
+        }
+        public void Display()
+        {
+            for (int i = 1; i <= uB; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz1 = new FizzBuzz(30);
+            fz1.Display();
+        }
+    }
+}
